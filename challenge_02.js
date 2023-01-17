8515f922b4a5d1cd25c1f983565cc9f09ceff267
Lawrence <lawrence.langen@gmail.com>
2023-01-17 09:30:05 +0100

    Switch Version

--- challenge_02.js
+++ challenge_02.js
@@ -17,28 +17,49 @@ Alter 13-17 // Cola
 /* Getränke-Challenge */
 
 
+// let namen, alter;
+
+// namen = (prompt("Bitte einen Namen eingeben"));
+// alter = (prompt("Bitte ein Alter eingeben")); 
+
+
+// if (alter >=  0 && alter < 6)
+// {
+//     console.log(namen + " " + "trinkt Milch");
+// }
+// else if (alter >= 6 && alter <= 12) 
+// {
+//     console.log(namen + " " + "trinkt Saft");
+// } 
+//     else if (alter >= 13 && alter <= 17 ) 
+//     {
+//     console.log(namen + " " + "trinkt Cola");
+//     } 
+//     else
+//     {
+//     console.log(namen + " " + "trinkt Wein");
+//     } 
+
+//*************  Switch Version **********/
+
 let namen, alter;
 
 namen = (prompt("Bitte einen Namen eingeben"));
 alter = (prompt("Bitte ein Alter eingeben")); 
 
-
-if (alter >=  0 && alter < 6)
-{
-    console.log(namen + " " + "trinkt Milch");
+switch (true) {
+    case (alter >=  0 && alter < 6): // true && true
+        console.log(namen + " " + "trinkt Milch");
+        break;
+    case (alter >= 6 && alter <= 12):
+        console.log(namen + " " + "trinkt Saft");
+        break;
+    case (alter >= 13 && alter <= 17):
+            console.log(namen + " " + "trinkt Cola");
+            break;
+    default:
+        console.log(namen + " " + "trinkt Wein");
+    //     console.log("a hat einen anderen Wert");
+        break;
 }
-else if (alter >= 6 && alter <= 12) 
-{
-    console.log(namen + " " + "trinkt Saft");
-} 
-    else if (alter >= 13 && alter <= 17 ) 
-    {
-    console.log(namen + " " + "trinkt Cola");
-    } 
-    else
-    {
-    console.log(namen + " " + "trinkt Wein");
-    } 
-
-
 
